51f29b7799fa2477077f14095e3bc454dab2275f
Gabriel A. Salinas Szada <75760163+gass-git@users.noreply.github.com>
2024-03-16 21:17:21 +0100

    new pieces of different colours are now being added to the board once the current active piece has landed

--- tile_map.gd
+++ tile_map.gd
@@ -6,16 +6,60 @@ extends TileMap
 # 
 # TODO write a concise explanation for both
 
-var speed = 1
-var tile_set_id = 1
-var tile_id = 1
-var board_layer_id = 0
-var active_layer_id = 1
-var piece_atlas: Vector2
+var pieces = [
+	{
+		"type": "tile", 
+		"color": "aqua",
+		"atlas_coordinates": Vector2i(0,0)	
+	},
+	{
+		"type": "tile", 
+		"color": "purple",
+		"atlas_coordinates": Vector2i(1,0)	
+	},
+	{
+		"type": "tile", 
+		"color": "yellow",
+		"atlas_coordinates": Vector2i(2,0)	
+	},
+	{
+		"type": "tile", 
+		"color": "red",
+		"atlas_coordinates": Vector2i(3,0)	
+	},
+	{
+		"type": "tile", 
+		"color": "green",
+		"atlas_coordinates": Vector2i(4,0)	
+	},
+	{
+		"type": "tile", 
+		"color": "brown",
+		"atlas_coordinates": Vector2i(5,0)	
+	},
+	{
+		"type": "tile", 
+		"color": "blue",
+		"atlas_coordinates": Vector2i(6,0)	
+	},
+	{
+		"type": "tile", 
+		"color": "grey",
+		"atlas_coordinates": Vector2i(7,0)	
+	}
+]
 
-var pos = {
-	"init": Vector2i(10, 1),
-	"current": null
+var active_piece = {
+	"initial_position": Vector2i(10, 1),
+	"current": {
+		"index": null,
+		"pos": null
+	}
+}
+
+var layer = {
+	"board": {"id": 0},
+	"active": {"id": 1}
 }
 
 var frames = {
@@ -26,8 +70,8 @@ var frames = {
 
 # called when the node enters the scene tree for the first time.
 func _ready():
-	pos.current = pos.init
-
+	active_piece.current.pos = active_piece.initial_position
+	active_piece.current.index = get_random_piece_index()
 
 # NOTE
 # -> called every frame.
@@ -35,17 +79,19 @@ func _ready():
 func _process(delta):
 	handle_movement()
 
+func get_random_piece_index():
+	return randi() % len(pieces)
 
 func handle_movement():
 	if Input.is_action_pressed("move_right") && frames.right.isMovable:
-		erase_cell(active_layer_id, pos.current)
-		pos.current.x += 1
+		erase_cell(layer.active.id, active_piece.current.pos)
+		active_piece.current.pos.x += 1
 		frames.right.count = 0
 		frames.right.isMovable = false
 	
 	if Input.is_action_pressed("move_left") && frames.left.isMovable:
-		erase_cell(active_layer_id, pos.current)
-		pos.current.x -= 1
+		erase_cell(layer.active.id, active_piece.current.pos)
+		active_piece.current.pos.x -= 1
 		frames.left.count = 0
 		frames.left.isMovable = false
 	
@@ -53,15 +99,15 @@ func handle_movement():
 		frames.down.count += 10
 	
 	if frames.down.isMovable:
-		erase_cell(active_layer_id, pos.current)	
+		erase_cell(layer.active.id, active_piece.current.pos)	
 		
-		if no_obstacle(): pos.current.y += speed
+		if no_obstacle(): active_piece.current.pos.y += 1
 		else: handle_land()
 			
 		frames.down.count = 0
 		frames.down.isMovable = false
 	
-	set_cell(active_layer_id, pos.current, tile_set_id, Vector2i(2,0))
+	set_cell(layer.active.id, active_piece.current.pos, 1, pieces[active_piece.current.index].atlas_coordinates)
 	handle_frame_count()	
 	
 func handle_frame_count():
@@ -69,13 +115,13 @@ func handle_frame_count():
 		if f.count < f.required_for_move: f.count += 1
 		elif !f.isMovable: f.isMovable = true 
 
-
 func no_obstacle():
-	return get_cell_source_id(board_layer_id, pos.current + Vector2i(0,1)) == -1	
+	return get_cell_source_id(layer.board.id, active_piece.current.pos + Vector2i(0,1)) == -1	
 
 func handle_land():
-	erase_cell(active_layer_id, pos.current)
-	set_cell(board_layer_id, pos.current, tile_set_id, Vector2i(2,0))
+	erase_cell(layer.active.id, active_piece.current.pos)
+	set_cell(layer.board.id, active_piece.current.pos, 1, pieces[active_piece.current.index].atlas_coordinates)
 	
 	# NOTE updates the current position to the initial position.
-	pos.current = pos.init
+	active_piece.current.pos = active_piece.initial_position
+	active_piece.current.index = get_random_piece_index()
