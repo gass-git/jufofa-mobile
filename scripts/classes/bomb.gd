c3e1f47ae00b8ff6ece33130947118ec0166f880
Gabriel Salinas Szada <gsalinas@itemis.com>
2024-09-29 18:09:10 +0200

    introducing classes

---
+++ scripts/classes/bomb.gd
@@ -0,0 +1,4 @@
+class_name Bomb extends Node
+
+var atlas: Vector2i = Vector2i(6,0)
+var source_id: int = 1
---
+++ scripts/classes/brick.gd
@@ -0,0 +1,13 @@
+class_name Brick extends Node
+
+# "vertical" or "horizontal"
+var orientation: String
+var source_id: int = 2
+var vertical_atlas: Array[Vector2i] = [Vector2i(2,0), Vector2i(1,0), Vector2i(0,0)]
+var horizontal_atlas: Array[Vector2i] = [Vector2i(3,0), Vector2i(4,0), Vector2i(5,0)]
+var shattered: bool = false
+
+func _init(initial_orientation):
+	orientation = initial_orientation
+
+func shatter(): shattered = true
